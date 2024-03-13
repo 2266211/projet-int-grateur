b3759d95038353e898849c1f1dd1067c48223399
2266211 <157629146+2266211@users.noreply.github.com>
2024-03-13 11:07:43 -0400

    Add files via upload
    intégration des modèles

--- QUIZ_LAB/routes/users.js
+++ QUIZ_LAB/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('C:/Users/2266211/Downloads/projet-int-grateur-main/projet-int-grateur-main/QUIZ_LAB/models/user');
 
 
 // Login Page
@@ -10,6 +11,27 @@ router.get('/connexion', (req, res) => {
 // Register Page
 router.get('/inscription', (req, res) => {
     res.render('inscription');
+});
+
+router.post('/connexion', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+
+        console.log('hop');
+
+        if (!user || user.password !== password) {
+            console.log('hopla');
+            return res.redirect('/users/connexion');
+        }
+
+        console.log('hoppla');
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
