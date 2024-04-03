2b8e0d7c221c8db856b40291eb7ffa1a54b53968
2266211 <157629146+2266211@users.noreply.github.com>
2024-04-03 09:50:18 -0400

    Add files via upload

--- QUIZ_LAB/routes/users.js
+++ QUIZ_LAB/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('C:/Users/dlour/Downloads/projet-int-grateur-compass-expressjs (1)/projet-int-grateur-compass-expressjs/QUIZ_LAB/models/user');
+const User = require('../models/user');
 
 
 module.exports = router;
