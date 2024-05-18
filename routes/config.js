a4bd5a919762c66d276cf3044c9a0460f5f9bc02
2266211 <157629146+2266211@users.noreply.github.com>
2024-05-18 10:45:27 -0400

    + commentaires EJS + page de profil serveur

---
+++ routes/config.js
@@ -0,0 +1,11 @@
+//Connexion avec la base de donnée MongoDB via Compass
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb+srv://Cluster:1234@cluster.rsbyhvp.mongodb.net/QUIZLAB");
+
+//Vérifier que la base de donnée est bien connectée
+connect.then(() => {
+    console.log("Base de donnée oppérationnelle");
+})
+.catch(() => {
+    console.log("Erreur au niveau de la base de donnée");
+});
